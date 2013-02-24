fd6870001525569f106de2b159e87886bcccb20c
Michael Ambrus <mambrus@gmail.com>
2013-02-24 13:50:39 +0100

    Year corrected in headers

--- sampler.c
+++ sampler.c
@@ -1,5 +1,5 @@
 /***************************************************************************
- *   Copyright (C) 2012 by Michael Ambrus                                  *
+ *   Copyright (C) 2013 by Michael Ambrus                                  *
  *   ambrmi09@gmail.com                                                    *
  *                                                                         *
  *   This program is free software; you can redistribute it and/or modify  *
