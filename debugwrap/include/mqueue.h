80cb5d8eddde6864bb859964bb29757b2f5108d5
Michael Ambrus <ambrmi09@gmail.com>
2013-03-11 18:50:39 +0100

    Tidied up the wrappers
    Category: administration

--- debugwrap/include/mqueue.h
+++ debugwrap/include/mqueue.h
@@ -1,6 +1,6 @@
 /***************************************************************************
- *   Copyright (C) 2006 by Michael Ambrus                                  *
- *   michael.ambrus@maquet.com                                             *
+ *   Copyright (C) 2013 by Michael Ambrus                                  *
+ *   ambrmi09@gmail.com                                             *
  *                                                                         *
  *   This program is free software; you can redistribute it and/or modify  *
  *   it under the terms of the GNU General Public License as published by  *
@@ -17,264 +17,59 @@
  *   Free Software Foundation, Inc.,                                       *
  *   59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.             *
  ***************************************************************************/
-/*!
-@file
-@ingroup POSIX_RT
 
-@brief RT queues - POSIX 1003.1b API
-
-\code
-This file was donated courtesy of the Zoi project (year 2000,
-SIEMENS Elema). It was originally written as a wrapper for
-pThreads-win32, to give Windows threads the ability of POSIX RT queues.
-\endcode
-
-For in-depth discussions about this component, see \ref
-POSIX_RT
-
-@see POSIX_RT
-@see PTHREAD
-*/
- 
 #ifndef _MQUEUE_H
 #define _MQUEUE_H
 
-#if defined(__GNUC__)
-   #include <tinker/config.h>
-   #if (!defined(TK_COMP_POSIX_RT) || TK_COMP_POSIX_RT==0 ) && !defined(ECODES_ONLY)
-   #error "mqueue.h" belongs to a component that your build of TinKer didn't include. Please reconfigure and rebuild TinKer.
-   #endif
-#endif
-/*****************************************************************************
- * include files
- *****************************************************************************/
-#include <sys/types.h>
-#include <sys/fcntl.h>
-#include <stddef.h>
+#include <tk_ansi_dirwrap.h>
+#include BUILDCHAIN(mqueue.h)
 
-/*****************************************************************************
- * definitions
- *****************************************************************************/
- /*****************************************************************************
- * definitions
- *****************************************************************************/
-typedef int mqd_t;
-
-//#ifdef _MSVC_
-#if defined(_WIN32) &&  defined(_MSC_VER)
-#  define HAVE_MODE_T     //!< This might be obsolete. Used to overcome glitch in pThreads W32 semaphore.h
-#  define EMSGSIZE     0x7F00
-#   if !defined(TINKER)
-       typedef int mode_t;     /*Belonging bits below*/
-#   endif
-#else
-
-#endif
-
-struct mq_attr{
-   size_t mq_msgsize;
-   size_t mq_maxmsg;
-};
- 
-/*mq_close*/
-/*EBADF */
-
-/*mq_getattr*/
-/*EBADF */
-
-/*mq_open*/
-
-
-
-/*
-EXIST
-ENAMETOOLONG 
-ENOENT
-ENOSPC
-*/
-
-/*mq_receive*/
-/*
-EAGAIN
-EBADF
-EMSGSIZE
-*/
-
-/* mq_send */
-/*
-EAGAIN
-EBADF
-EMSGSIZE
-EINVAL
-*/
-
-/* mq_unlink */
-/*ENOENT*/
-
-/*****************************************************************************
- * public function declarations
- *****************************************************************************/
-
-#include <tinker/config.h>
-#if defined(__cplusplus) && (TK_CPLUSPLUS == 0)
+#if defined(__cplusplus)
 extern "C" {
 #endif
 
-int mq_close(
+int X_mq_close(
    mqd_t                 mq
 );
 
-int mq_getattr(
+int X_mq_getattr(
    mqd_t                mq,
    struct mq_attr      *attrbuf
 );
 
-mqd_t mq_open(
+mqd_t X_mq_open(
    const char           *mq_name,
    int                   oflags,
    mode_t                mode,
    struct mq_attr       *mq_attr
 );
 
-size_t mq_receive(
+size_t X_mq_receive(
    mqd_t                 mq,
    char                 *msg_buffer,
    size_t                buflen,
    unsigned int         *msgprio
 );
 
-int mq_setattr(
+int X_mq_setattr(
    mqd_t                 mqdes,
    const struct mq_attr *new_attrs,
    struct mq_attr       *old_attrs
 );
 
+#define mq_send     X_mq_send
+#define mq_unlink   X_mq_unlink
+#define mq_close    X_mq_close
+#define mq_getattr  X_mq_getattr
+#define mq_open     X_mq_open
+#define mq_receive  X_mq_receive
+#define mq_setattr  X_mq_setattr
+#define mq_send     X_mq_send
+#define mq_unlink   X_mq_unlink
 
-int mq_send(
-   mqd_t                 mq,
-   const char           *msg,
-   size_t                msglen,
-   unsigned int          msgprio
-);
-
-int mq_unlink(
-   const char           *mq_name
-);
-
-#if defined(__cplusplus) && (TK_CPLUSPLUS == 0)
+#if defined(__cplusplus)
 }
 #endif
 
 #endif
-
-
-/*
-//Compact format so that TiNa can find the functions
-int mq_close(mqd_t mq);
-int mq_getattr(mqd_t mq, struct mq_attr *attrbuf);
-mqd_t mq_open(const char *mq_name, int oflags,mode_t mode,struct mq_attr *mq_attr);
-size_t mq_receive(mqd_t mq,char *msg_buffer,size_t buflen,unsigned int *msgprio);
-int mq_setattr(mqd_t mqdes, const struct mq_attr *new_attrs,struct mq_attr *old_attrs);
-int mq_send(mqd_t mq, const char *msg, size_t msglen, unsigned int msgprio);
-int mq_unlink(const char *mq_name);
-*/
-
-/*!
- * @defgroup CVSLOG_mqueue_h mqueue_h
- * @ingroup CVSLOG
- *  $Log: mqueue.h,v $
- *  Revision 1.12  2007-03-08 23:28:35  ambrmi09
- *  Minor changes made in TinKer headers for TiNa code ceneration to work
- *
- *  Revision 1.11  2007-03-03 23:01:32  ambrmi09
- *  Added driver support for FIFO's
- *
- *  Revision 1.10  2006-12-12 10:57:05  ambrmi09
- *  This adresses the second part of #1609064
- *
- *  Revision 1.9  2006/11/05 19:06:03  ambrmi09
- *  Buildsystem adjusted to permit configuration of components.
- *  Now when component is enabled it will also be included in the build
- *  (instead of just sanity-tested in the source files).
- *
- *  Also a feature for application sanity is assed. When a header-file is
- *  included in the application, a check against the component it belongs
- *  to will be performed. That way user don't need to rely on run-time
- *  checks and can get feedback much earlier.
- *
- *  Revision 1.8  2006/04/08 10:15:57  ambrmi09
- *  Merged with branch newThreadstarter (as of 060408)
- *
- *  Revision 1.7.2.1  2006/04/03 20:07:22  ambrmi09
- *  Minor cosmetic change
- *
- *  Revision 1.7  2006/03/17 12:20:02  ambrmi09
- *  Major uppdate (5 days hard work)
- *
- *  - Finally tied up all loose ends in the concept. Threads are now
- *  joinable
- *
- *  - Corrected one error: compacting scheduele while cancelling a
- *  threads
- *
- *  - Several new API, mainly concerned with cancelation (corrsp pThread
- *  also)
- *
- *  - Found a nasty bug while creating threads in threads for XC167. TOS is
- *  really a patchy solution ;( This one had to do with the compiler
- *  being fooled by the inline assembly and optimized something that was not
- *  optimizable (saving stack segment got wacked).
- *
- *  - Designed a concurrent qsort test-app. This is good for showing
- *  boss-worker model. Number of threads recoed on XC167 was 50 and on MSVS
- *  more than 150! Interesting to notice was that TinKer creation and
- *  cancelation for threads was much faster than Windows own (20-30 times
- *  faster).
- *
- *  - A MSVC workspace for pThreads-Win32. Good for testing apps
- *  transparency.
- *
- *  - Increased memory on XC167 (phyCore HW). now 32k for stacks and 16k for
- *  malloc. We still lack RAM that is not deployed (pHycore has
- *  128k + 256k physical RAM memory i think). Maximum for
- *  stack is 64k however (type of pointers determine this). If memory is
- *  increased further, we get a TRAP_B saying bad memory interface. Typical
- *  error for config memory issues in DaVe.
- *
- *  Revision 1.6  2006/03/05 11:11:24  ambrmi09
- *  License added (GPL).
- *
- *  Revision 1.5  2006/02/23 11:34:59  ambrmi09
- *  - Improved post mortem
- *   - Fixed bug in i2hex2.
- *   - Added uptime output
- *   - mark running among RDY threads in schedule dump
- *
- *  - \ref putchar now supports easy switching between serial0 and serial1
- *
- *  mqueue.h and mqueue.c should be untouched. But trying to identify if
- *  qsort is the reason for TinKer sometimes to hang (recent possible bug).
- *
- *  Revision 1.4  2006/02/22 13:05:45  ambrmi09
- *  Major doxygen structure modification. No chancge in actual sourcecode.
- *
- * Revision 1.4  2006/02/22 13:05:45  ambrmi09
- * Major doxygen structure modification. No chancge in actual sourcecode.
- *
- * Revision 1.3  2006/02/21 22:10:31  ambrmi09
- * - Added wrapper macro for pthread_create so that posix threads get named in
- *   TinKer (makes post-mortem easier). Very cool solution with a macro...
- * - Improved post-mortem, the schedule gets dumpt also now
- * - Wrapper macros for msleep and usleep (temporary)
- * - Minor stubbing and wrapping of mq_unlink and pthread_cancel
- * - Added a new test program (t est-posix.c ). This is verifyed to compile and
- *   run on both Linux and TinKer unmodified!
- *
- * Revision 1.2  2006/02/20 15:22:00  ambrmi09
- * Documentation stuff. No code changes.
- *
- * Revision 1.1  2006/02/17 21:16:16  ambrmi09
- * Initial commit: code donated by the Zoi project (author: Michael Ambrus)
- *
- */
 
--- debugwrap/include/tk_ansi_dirwrap.h
+++ debugwrap/include/tk_ansi_dirwrap.h
@@ -1,6 +1,6 @@
 /***************************************************************************
- *   Copyright (C) 2006 by Michael Ambrus                                  *
- *   michael.ambrus@maquet.com                                             *
+ *   Copyright (C) 2013 by Michael Ambrus                                  *
+ *   ambrmi09@gmail.com                                             *
  *                                                                         *
  *   This program is free software; you can redistribute it and/or modify  *
  *   it under the terms of the GNU General Public License as published by  *
@@ -18,64 +18,6 @@
  *   59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.             *
  ***************************************************************************/
 
-/*!
-@file
-@ingroup kernel_reimpl_ansi
-
-@brief ANSI functions reimplemented
-
-Special file for getting tool-chain head files.
-
-@note <b>This file is supposed to be included "several" times.</b> Hence 
-these is no "#ifndef" protection.
-
-For in-depth discussions about re-implementing ANSI functions, see \ref
-kernel_reimpl_ansi
-
-@see kernel_reimpl_ansi
-
-*/
-
-#ifndef YES
-#define YES 1
-#endif
-
-#ifndef NO
-#define NO 0
-#endif
-
-/*
-Make sure pthread stypes, time-constants and other stuff from other from any 
-of these OS'es that we don't care about. This will affect certain types and 
-macros both for the kernel, but also for any application using the kernel 
-header structure
-
-#if defined(__rtems__)
-#undef __rtems__
-#endif
-
-#if defined(__svr4__)
-#undef __svr4__
-#endif
-
-#if defined(__CYGWIN__)
-#undef __CYGWIN__
-#endif
-*/
-
-/*
-Below will do the same thing but it's safer (undefining the others is too crude 
-since they might be used for other datatypes like dev_t). 
-
-What the below setting does is preventing newlib headers to include
-the sys/features file, which is __rtems__ only specific and which sets
-_POSIX_THREADS among other pthread retated things.
-*/
-#ifndef _SYS_FEATURES_H
-#define _SYS_FEATURES_H
-#endif
-
-//------1---------2---------3---------4---------5---------6---------7---------8
 #if defined(_WIN32) &&  defined(_MSC_VER)
    #define CHAINPATH C:\Program Files\Microsoft Visual Studio\VC98\Include
 
@@ -83,7 +25,6 @@ _POSIX_THREADS among other pthread retated things.
    #define CHAINPATH C:/BC5/INCLUDE
 
 #elif defined( __C166__ )
-// #define CHAINPATH C:\Program Files\Keil\C166\INC
    #define CHAINPATH C:\Keil\C166\INC
 
 #elif defined(__GNUC__) /*|| defined(__CYGWIN32__)  || defined(__CYGWIN__)*/
@@ -91,38 +32,9 @@ _POSIX_THREADS among other pthread retated things.
       #define CHAINPATH /usr/include
    #endif
 
-//Getting rid of other build-ins that will interfear
-// NOTE Inspect tool-chain builtins by invoking:
-// echo | *-*-*-cpp -dM | less
-// Look for macros that don't have '__' prefix and suffix
-
-//The following workarounds are necessary for our BUILDCHAIN macro to work
-   #ifdef bfin
-      #warning Workaround enabled for GCC builtin macro bug: bfin
-      #undef bfin
-      #ifndef __bfin__
-         #define __bfin__
-      #endif
-   #endif
-   #ifdef BFIN
-      #warning Workaround enabled for GCC builtin macro bug: BFIN
-      #undef BFIN
-      #ifndef __BFIN__
-         #define __BFIN__
-      #endif
-   #endif
-   #ifdef i386
-//      #warning Workaround enabled for GCC builtin macro bug: i386
-      #undef i386
-      #ifndef __i386__
-         #define __i386__
-      #endif
-   #endif
-
 #else
-   #error "Can\'t determine the target for the TINKER kernel"
+   #error "Can\'t determine the target you're building for"
 #endif
-//------1---------2---------3---------4---------5---------6---------7---------8
 
 #define DEFSTR( x ) \
    #x
@@ -133,91 +45,3 @@ _POSIX_THREADS among other pthread retated things.
 #define BUILDCHAIN( file ) \
    FNAME( CHAINPATH, file ) \
 
-
-/*! 
- * @defgroup CVSLOG_tk_ansi_dirwrap_h tk_ansi_dirwrap_h
- * @ingroup CVSLOG
- *
- *  $Log: tk_ansi_dirwrap.h,v $
- *  Revision 1.6  2007-03-03 23:01:32  ambrmi09
- *  Added driver support for FIFO's
- *
- *  Revision 1.5  2007-02-26 14:16:53  ambrmi09
- *  1) Drivers    - structure added
- *  2) Filesystem - in progress
- *
- *  Revision 1.4  2007-02-24 14:27:16  ambrmi09
- *  * i386-hixs-elf compiles and links properly
- *
- *  Revision 1.3  2007-02-24 12:17:14  ambrmi09
- *  1) Sync PowerPC sources
- *  2) Add structure for i386-hixs-elf target. (Hopefully our new test-bed)
- *     for developping the filesys component.)
- *
- *  Revision 1.2  2007-02-19 17:34:37  ambrmi09
- *  Tinker will now compile for target powerpc-hixs-rtems
- *
- *  Revision 1.1  2006/11/21 17:19:39  ambrmi09
- *  Moved tk_ansi_dirwrap to public directory (applications without TinKer sources needs also wrapping)
- *
- *  Revision 1.8  2006/10/09 17:08:06  ambrmi09
- *  * Sceleton for bfin and powerpc now compiles.
- *  * Refingnemen in auto-tools to build correct BSP depending on toolchain and settings
- *  * BFIN,bfin macro work-around
- *
- *  Revision 1.7  2006/04/08 10:16:02  ambrmi09
- *  Merged with branch newThreadstarter (as of 060408)
- *
- *  Revision 1.6.2.2  2006/04/03 20:07:29  ambrmi09
- *  Minor cosmetic change
- *
- *  Revision 1.6.2.1  2006/04/03 15:21:48  ambrmi09
- *  All targets updated with the new thread-starter (alternative 2).
- *
- *  This alternative has one weakness (explained elsewhere togeather
- *  with alternative 1), but so far it's the only one that will compile
- *  and function equally among all 4 (very different) compilers currently
- *  tested against: GCC, MSVC, BC5 and Keil.
- *
- *  If nothing else turns up, I'm willing to overcome the drawback (it's
- *  quite handleable) because it *truly* takes away a lot of pain with
- *  porting.
- *
- *  The ARM port (architecture level) is than's to this now fully operational
- *  without the r13 hack in the context switch. This includes thread
- *  cancellation and thread argument passing (which were not functioning in
- *  the old port).
- *
- *  If this revised code proves itself (i.e. no surprises turns up) then
- *  TinKer can be considered "almost ported" to any HW target that GCC is
- *  ported for :D (/cheers)
- *
- *  Revision 1.6  2006/03/27 13:40:15  ambrmi09
- *  As part of the preparation for the first release, code has been cleaned up a little
- *  and project has been checked that it will build on all it's intended targets.
- *
- *  Problems that remained had to do the ANSI wrapping.
- *
- *  Some modifications were neserary to make the BC5 build, but the result is cleaner
- *  and more consistent with the rest of the wrapping. As a consequence, stdlib.h was
- *  introduced.
- *
- *  Revision 1.5  2006/03/05 11:11:27  ambrmi09
- *  License added (GPL).
- *
- *  Revision 1.4  2006/03/04 19:32:35  ambrmi09
- *  Modified sources to allow build GNU targets transparently.
- *
- *  Revision 1.3  2006/03/04 14:28:44  ambrmi09
- *  Finally got the \ref clock() representation right. Now timing is
- *  behaving equaly between the targets X86_Linux, Cygqing, MSVC, BC5 and
- *  XC167.
- *
- *  Revision 1.2  2006/03/02 14:05:50  ambrmi09
- *  Posting to GNU toolchain started
- *
- *  Revision 1.1  2006/02/28 18:40:11  ambrmi09
- *  initial ci
- *
- *
- */
